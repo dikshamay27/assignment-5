28b8f486e517ce83a23f476020dab9fa5c49692f
dikshamay27 <may27diksha@gmail.com>
2018-06-28 15:29:44 +0530

    file added

--- quiz1.py
+++
@@ -1,7 +0,0 @@
-#Question : Write a Phython program to calculate the area of parallelogram.
-
-base = float(input('Length of base: '))
-height = float(input('Measurement of height: '))
-area = base * height
-print("Area is: ", area)
-
